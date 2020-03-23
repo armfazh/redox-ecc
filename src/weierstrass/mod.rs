30c10b5c1d238927b60d7598be3192ae2206a13d
armfazh <armfazh@cloudflare.com>
2020-03-23 16:51:43 -0700

    Adding SSWU for curves with AB=0.
    Includes curves: BLS12381 and SECP256K1.

--- src/weierstrass/mod.rs
+++ src/weierstrass/mod.rs
@@ -6,10 +6,12 @@ mod curve;
 mod point;
 mod scalar;
 mod sswu;
+mod sswuab0;
 mod svdw;
 
 pub use crate::weierstrass::curve::{Curve, Params};
 pub use crate::weierstrass::point::{Point, ProyCoordinates};
 pub use crate::weierstrass::scalar::Scalar;
 pub use crate::weierstrass::sswu::SSWU;
+pub use crate::weierstrass::sswuab0::SSWUAB0;
 pub use crate::weierstrass::svdw::SVDW;
---
+++ src/weierstrass/sswuab0.rs
@@ -0,0 +1,44 @@
+use num_traits::identities::Zero;
+
+use crate::ellipticcurve::{EllipticCurve, Isogeny, MapToCurve};
+use crate::field::{Field, Sgn0Endianness};
+use crate::primefield::FpElt;
+use crate::weierstrass::{Curve, SSWU};
+
+pub struct SSWUAB0 {
+    // e: Curve,
+    iso: Box<dyn Isogeny<E0 = Curve, E1 = Curve>>,
+    sswu: Box<dyn MapToCurve<E = Curve>>,
+}
+
+impl SSWUAB0 {
+    pub fn new(
+        e: Curve,
+        z: FpElt,
+        sgn0: Sgn0Endianness,
+        iso: Box<dyn Isogeny<E0 = Curve, E1 = Curve>>,
+    ) -> SSWUAB0 {
+        if !SSWUAB0::verify(&e, &iso) {
+            panic!("wrong input parameters")
+        } else {
+            let sswu = Box::new(SSWU::new(iso.domain(), z, sgn0));
+            SSWUAB0 { iso, sswu }
+        }
+    }
+    fn verify(e: &Curve, iso: &Box<dyn Isogeny<E0 = Curve, E1 = Curve>>) -> bool {
+        let cond0 = *e == iso.codomain();
+        let cond1 = e.a.is_zero(); // A == 0
+        let cond2 = e.b.is_zero(); // B == 0
+        cond0 && (cond1 ^ cond2) // A == 0 xor B == 0
+    }
+}
+
+impl MapToCurve for SSWUAB0 {
+    type E = Curve;
+    fn map(
+        &self,
+        u: <<Self::E as EllipticCurve>::F as Field>::Elt,
+    ) -> <Self::E as EllipticCurve>::Point {
+        self.iso.push(self.sswu.map(u))
+    }
+}
